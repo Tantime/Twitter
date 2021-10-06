3721a2d06085ae1674b039220003471ace4649b1
Tantime <48768802+Tantime@users.noreply.github.com>
2021-10-05 17:25:27 -0700

    user stays logged in across restarts; user can view tweets w/ picture, name, and tweet content

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        loginButton.layer.cornerRadius = 10.0
         // Do any additional setup after loading the view.
     }
     
